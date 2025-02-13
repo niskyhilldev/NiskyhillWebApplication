84c1872f9c2073074d7be71b53e6bf8f84dcc3aa
Tedd Stabolepszy <teddstabolepszy@dyn245239.cc.lehigh.edu>
2025-02-13 16:01:03 -0500

    updated git ignore and bash script to copy files from Web branch

---
+++ Server/server.java/pullWebFiles.bash
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+
+# This Bash Script pulls all files from the Web Branch of the BAM repo so the server can add them in the route
+# It's purpose is get the latest version of the Front End "Files" into the folder defined in SERVER.JAVA
+
+
+
+# Git Repo Information below (Stored in plain Text?)
+# WARNING: You will need to accept permission to allow your IDE to access git through HTTP Pull requests
+REPO_URL=${1:-"https://github.com/tjs226/BAM_Niskey_Hill.git"}
+BRANCH_NAME=${2:-"Web"}
+TARGET_DIR="$(pwd)/src/main/resources/public"
+
+
+
+
+TMP_DIR=$(mktemp -d)
+git clone --branch "$BRANCH_NAME" --single-branch "$REPO_URL" "$TMP_DIR"
+if [ $? -ne 0 ]; then
+    echo "Failed to clone repository."
+    exit 1
+fi
+
+
+mkdir -p "$TARGET_DIR"
+cp -r "$TMP_DIR"/* "$TARGET_DIR"/
+rm -rf "$TMP_DIR"
+
+
+
+echo "Files from branch '$BRANCH_NAME' of repository '$REPO_URL' have been copied to '$TARGET_DIR'."
