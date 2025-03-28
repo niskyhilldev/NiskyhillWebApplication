94ea053b0f3246945fcf0681cfef6a1302752fe7
Tedd Stabolepszy <teddstabolepszy@Tedds-MacBook-Pro.local>
2025-03-27 21:55:09 -0400

    added port binding

--- Server/server.java/src/main/java/moravians/niskyhill/server/Server.java
+++ Server/server.java/src/main/java/moravians/niskyhill/server/Server.java
@@ -1,55 +1,55 @@
 package moravians.niskyhill.server;
 
-// create an HTTP GET route
+// Import Javalin and Gson
 import io.javalin.Javalin;
 import io.javalin.http.staticfiles.Location;
 import com.google.gson.*;
 
 /**
- * Server For Nisky Hill Website 
+ * Server for Nisky Hill Website
  */
 public class Server {
 
-    /* Port for the Server to use */
-    public static final int PORT_WEBSERVER = 8080;
-
-
-
-
     public static void main(String[] args) {
 
-
         /*
-        * Create the Instance of the Javelin Server
-        */
-            Javalin app = Javalin.create(config -> {
+         * Create the Instance of the Javalin Server
+         */
+        Javalin app = Javalin.create(config -> {
 
-                /* Terminal Logger to manage requests */
-                config.requestLogger.http((ctx, ms) -> {
-                    System.out.printf("%s\t%s\t%s\n", ctx.method(), ctx.path(), ctx.status());
-                });
-
-                /* Place to hold the Static HTML and CSS Files */
-                config.staticFiles.add(staticFiles -> {
-                    staticFiles.hostedPath = "/";
-                    staticFiles.directory = "/public"; 
-                    staticFiles.location = Location.CLASSPATH;
-                });
+            /* Terminal Logger to manage requests */
+            config.requestLogger.http((ctx, ms) -> {
+                System.out.printf("%s\t%s\t%s\n", ctx.method(), ctx.path(), ctx.status());
             });
 
-
-
-
+            /* Place to hold the Static HTML and CSS Files */
+            config.staticFiles.add(staticFiles -> {
+                staticFiles.hostedPath = "/";
+                staticFiles.directory = "/public"; 
+                staticFiles.location = Location.CLASSPATH;
+            });
+        });
 
         /*
          * HTTP Routes 
-        */
-        final Gson gson = new Gson(); 
+         */
+        final Gson gson = new Gson();
 
 
 
+
+
+
+
+
+
+
+
+        // Get port from environment (Heroku) or default to 8080 (for local deployment)
+        String port = System.getenv("PORT");
+        int serverPort = (port != null) ? Integer.parseInt(port) : 8080;
 
         /* Start the Server */
-        app.start(PORT_WEBSERVER);
+        app.start(serverPort);
     }
 }
