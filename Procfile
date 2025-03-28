6c00bc7556cb7e4c6238dba834030b7d77b9760b
Tedd Stabolepszy <teddstabolepszy@Tedds-MacBook-Pro.local>
2025-03-27 21:38:14 -0400

    files ready for deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JAVA_OPTS -cp target/server.java-1.0-SNAPSHOT-jar-with-dependencies.jar moravians.niskyhill.server.Server
---
+++ pullWebFiles.bash
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# This Bash Script pulls all files from the Web Branch of the BAM repo so the server can add them in the route
+# Its purpose is to get the latest version of the Front End "Files" into the folder defined in SERVER.JAVA
+# NOTE: FRONT END FILES ARE NOT STORED ON THE SERVER BRANCH, clone and run this script to get them
+
+# Git Repo Information below (Stored in plain text?)
+# WARNING: You will need to accept permission to allow your IDE to access git through HTTP Pull requests
+
+
+REPO_URL=${1:-"https://github.com/tjs226/BAM_Niskey_Hill.git"}
+BRANCH_NAME=${2:-"Web"}
+TARGET_DIR="$(pwd)/src/main/resources/public"
+
+TMP_DIR=$(mktemp -d)
+git clone --branch "$BRANCH_NAME" --single-branch "$REPO_URL" "$TMP_DIR"
+if [ $? -ne 0 ]; then
+    echo "Failed to clone repository."
+    exit 1
+fi
+
+# Remove all files in the target directory before copying
+rm -rf "$TARGET_DIR"/*
+
+mkdir -p "$TARGET_DIR"
+cp -r "$TMP_DIR"/* "$TARGET_DIR"/
+
+find "$TARGET_DIR" -type f -name "*.md" -delete # Remove all .md files from the target directory
+
+rm -rf "$TMP_DIR"
+
+echo "Files from branch '$BRANCH_NAME' of repository '$REPO_URL' have been copied to '$TARGET_DIR'."
