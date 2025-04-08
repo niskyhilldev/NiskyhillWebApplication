08fa251fef28bfbf0ab15e321c02796e327d1d8c
matthew0122 <mdb326@lehigh.edu>
2025-04-08 13:54:13 -0400

    Add a resident

--- admin/admin.js
+++ admin/admin.js
@@ -533,6 +533,10 @@ function toggleForm() {
     const form = document.getElementById('addOwnerForm');
     form.style.display = form.style.display === 'none' ? 'block' : 'none';
 }
+function toggleResForm() {
+    const form = document.getElementById('addResidentForm');
+    form.style.display = form.style.display === 'none' ? 'block' : 'none';
+}
 function toggleLotForm() {
     const form = document.getElementById('addLotForm');
     form.style.display = form.style.display === 'none' ? 'block' : 'none';
@@ -578,6 +582,49 @@ document.getElementById('newOwnerForm').addEventListener('submit', function(even
     document.getElementById('newOwnerForm').reset();
 
     toggleForm(); // Hide the form after submission
+});
+document.getElementById('newResidentForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    // Generate a new unique ID based on the highest existing key in data
+    const newId = Object.keys(data).length > 0 
+        ? Math.max(...Object.keys(data).map(Number)) + 1 
+        : 0;
+
+    // Get all the values from the form
+    const firstName = document.getElementById('fName').value;
+    const middleName = document.getElementById('mName').value;
+    const lastName = document.getElementById('lName').value;
+    const lotNumber = document.getElementById('resLotNum').value;
+    const lotPortion = document.getElementById('resPortion').value;
+    const section = document.getElementById('resCemSection').value;
+    const notes = document.getElementById('resNote').files[0] ? document.getElementById('note').files[0].name : '';
+    const organization = document.getElementById('resOrganization').value;
+    const dob = document.getElementById('dofb').value;
+    const dod = document.getElementById('dofd').value;
+    const vessel = document.getElementById('vesselType').value;
+    
+    // Add new entry to the data object
+    data[newId] = {
+        lotNumber: lotNumber,
+        lotId: lotPortion,
+        section: section,
+        buriedFirst: firstName,
+        buriedMiddle: middleName,
+        buriedLast: lastName,
+        dob: dob,
+        dod: dod,
+        vessel: vessel,
+        owns: false,
+        notes: notes,
+        organization: organization
+    };
+
+    
+    // Clear the form fields
+    document.getElementById('newResidentForm').reset();
+
+    toggleResForm(); // Hide the form after submission
 });
 document.getElementById('newLotForm').addEventListener('submit', function(event) {
     event.preventDefault();
