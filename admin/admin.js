0148c5ed6cf773aedf0c0a407463bef39ed891b9
matthew0122 <mdb326@lehigh.edu>
2025-09-23 15:35:54 -0400

    add_lot separation

--- admin/admin.js
+++ admin/admin.js
@@ -662,10 +662,6 @@ function toggleResForm() {
     const form = document.getElementById('addResidentForm');
     form.style.display = form.style.display === 'none' ? 'block' : 'none';
 }
-function toggleLotForm() {
-    const form = document.getElementById('addLotForm');
-    form.style.display = form.style.display === 'none' ? 'block' : 'none';
-}
 function toggleSectionForm() {
     const form = document.getElementById('addSectionForm');
     form.style.display = form.style.display === 'none' ? 'block' : 'none';
@@ -679,38 +675,7 @@ function toggleFileForm() {
 
 
 
-document.getElementById('newLotForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    
-    // Generate a new unique ID based on the highest existing key in data
-    const newId = Object.keys(plotsData).length > 0 
-        ? Math.max(...Object.keys(plotsData).map(Number)) + 1 
-        : 0;
 
-    // Get all the values from the form
-    const lotNumber = document.getElementById('addLotNumber').value;
-    const lotPortion = document.getElementById('addLotPortion').value;
-    const section = document.getElementById('sectionId').value;
-    const ownerFirst = document.getElementById('ownerFirst').value;
-    const ownerMiddle = document.getElementById('ownerMiddle').value;
-    const ownerLast = document.getElementById('ownerLast').value;
-    const record = document.getElementById('record').files[0] ? document.getElementById('note').files[0].name : '';
-    
-    // Add new entry to the data object
-    plotsData[newId] = {
-        lotNumber: lotNumber,
-        lotPartition: lotPortion,
-        section: section,
-        owner: `${ownerFirst || ""} ${ownerMiddle || ""} ${ownerLast || ""}`,
-        internmentRecord: record,
-    };
-
-    
-    // Clear the form fields
-    document.getElementById('newLotForm').reset();
-
-    toggleLotForm(); // Hide the form after submission
-});
 document.getElementById('newSectionForm').addEventListener('submit', function(event) {
     event.preventDefault();
     
