23b2b5b65e58603d5891b32373158b221a7a8995
matthew0122 <mdb326@lehigh.edu>
2025-09-23 15:30:41 -0400

    add_resident separation

--- admin/admin.js
+++ admin/admin.js
@@ -678,51 +678,7 @@ function toggleFileForm() {
 
 
 
-document.getElementById('newResidentForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    
-    // Generate a new unique ID based on the highest existing key in data
-    const newId = Object.keys(data).length > 0 
-        ? Math.max(...Object.keys(data).map(Number)) + 1 
-        : 0;
 
-    // Get all the values from the form
-    const firstName = document.getElementById('fName').value;
-    const middleName = document.getElementById('mName').value;
-    const lastName = document.getElementById('lName').value;
-    const lotNumber = document.getElementById('resLotNum').value;
-    const lotPortion = document.getElementById('resPortion').value;
-    const section = document.getElementById('resCemSection').value;
-    // const notes = document.getElementById('resNote').files[0] ? document.getElementById('note').files[0].name : '';
-    const organization = document.getElementById('resOrganization').value;
-    const dob = document.getElementById('dofb').value;
-    const dod = document.getElementById('dofd').value;
-    const vessel = document.getElementById('vesselType').value;
-    const valid = document.getElementById('valid').checked;
-    
-    // Add new entry to the data object
-    data[newId] = {
-        lotNumber: lotNumber,
-        lotId: lotPortion,
-        section: section,
-        buriedFirst: firstName,
-        buriedMiddle: middleName,
-        buriedLast: lastName,
-        dob: dob,
-        dod: dod,
-        vessel: vessel,
-        valid: valid,
-        owns: false,
-        // notes: notes,
-        organization: organization
-    };
-
-    
-    // Clear the form fields
-    document.getElementById('newResidentForm').reset();
-
-    toggleResForm(); // Hide the form after submission
-});
 document.getElementById('newLotForm').addEventListener('submit', function(event) {
     event.preventDefault();
     
