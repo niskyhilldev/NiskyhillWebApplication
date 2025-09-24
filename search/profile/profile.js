f9c81c6debcd5124478004b66d4a597628bd65af
thc225 <thc225@lehigh.edu>
2025-09-24 17:14:30 -0400

    added in/connected profile page to db

--- search/profile/profile.js
+++ search/profile/profile.js
@@ -1,7 +1,46 @@
 document.addEventListener('DOMContentLoaded', () => {
     let searchParams = new URLSearchParams(window.location.search);
     let userID = searchParams.get("id")  //search the URL for the team ID
+    const API_BASE_URL = 'http://localhost:8080/residents/find'
+    fetchResident();
 
-    document.getElementById("userID").innerHTML = userID
+    
+    async function fetchResident() {
+        try{
+            const response = await fetch(`${API_BASE_URL}/${userID}`)
+            if (!response.ok) {
+                    if (response.status === 404) {
+                        throw new Error(`Resident with ID ${rid} not found`);
+                    } else if (response.status === 400) {
+                        throw new Error('Invalid Resident ID format');
+                    } else if (response.status === 500) {
+                        throw new Error('Server error occurred');
+                    } else {
+                        throw new Error(`HTTP ${response.status}: ${response.statusText}`);
+                    }
+            }
+
+            const resident = await response.json();
+            displayResident(resident)
+
+
+        }catch(err){
+            console.log('Error fetching resident:', err);
+        }
+    }
+
+    function displayResident(resident){
+
+        const name = (resident.firstName || '') + ' ' + (resident.middleName || '') + ' ' + (resident.lastName || '')
+
+        document.getElementById("name").innerHTML = name
+        document.getElementById("burialDate").innerHTML = (resident.burialDate || 'Not available')
+        document.getElementById("section").innerHTML = (resident.lot.section.name || 'Not available')
+        document.getElementById("lot").innerHTML = (resident.lot.number || 'Not available')
+        document.getElementById("lotOwner").innerHTML = (resident.lot.owner || 'Not available')
+        document.getElementById("lotDescriptor").innerHTML = (resident.lot.descriptor || 'Not available')
+        document.getElementById("residentID").innerHTML = (resident.rid || 'Not available')
+    }
+
 
 });
