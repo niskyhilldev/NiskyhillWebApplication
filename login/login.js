6e9300495a9b3497a14910e91e8cd3f844e7cd56
Tedd Stabolepszy <143549208+tjs226@users.noreply.github.com>
2025-10-30 17:47:36 -0400

    added base url

--- login/login.js
+++ login/login.js
@@ -1,3 +1,6 @@
+API_BASE_URL = 'https://niskyhillcemetery-23a1ead2d9b1.herokuapp.com';
+
+
 document.addEventListener("DOMContentLoaded", () => {
     const form = document.getElementById("loginForm");
     const username = document.getElementById("username");
@@ -19,7 +22,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
         try {
             // Call the backend login API
-            const response = await fetch('https://www.niskyhill.org/auth/login', {
+            const response = await fetch(`${API_BASE_URL}/auth/login`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
