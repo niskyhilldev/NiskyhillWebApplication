0dcc0a248ba06fb5ba489a0fdba8d6bcaf5da1e9
matthew0122 <mdb326@lehigh.edu>
2025-10-30 16:38:43 -0400

    Update url

--- admin/update_password/updatepassword.js
+++ admin/update_password/updatepassword.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    API_BASE_URL = 'https://niskyhillcemetery-23a1ead2d9b1.herokuapp.com/';
+    API_BASE_URL = 'https://niskyhillcemetery-23a1ead2d9b1.herokuapp.com';
 
     checkSessionAndSetRedirect()
 
