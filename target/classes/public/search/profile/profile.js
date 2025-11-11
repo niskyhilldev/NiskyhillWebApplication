cd489f565c0485b6f03b3f183d6d5f650caddbc5
Tedd Stabolepszy <143549208+tjs226@users.noreply.github.com>
2025-11-11 15:58:13 -0500

    fixed Thomas's bad url

--- target/classes/public/search/profile/profile.js
+++ target/classes/public/search/profile/profile.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const qParam  = searchParams.get("q");
   const latParam = parseFloat(searchParams.get("lat"));
   const lngParam = parseFloat(searchParams.get("lng"));
-  const API_BASE_URL = 'https://niskyhill.org';
+  const API_BASE_URL = 'https://www.niskyhill.org';
 
   // ===== Map state =====
   let map;
