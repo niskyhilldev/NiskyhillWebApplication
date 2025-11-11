1ec8b6e88df9f13a1b52b8a312366f6b285f4d66
sjm225 <sjm225@lehigh.edu>
2025-11-11 15:12:31 -0500

    sections

--- search/profile/profile.js
+++ search/profile/profile.js
@@ -57,7 +57,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // ===== DEBUG: Show all sections (comment out when not needed) =====
   // Show ALL sections:
-  //  Object.values(sectionLayers || {}).forEach(({route, outline}) => { if(route) route.addTo(map); if(outline) outline.addTo(map); });
+   Object.values(sectionLayers || {}).forEach(({route, outline}) => { if(route) route.addTo(map); if(outline) outline.addTo(map); });
   
   // Show SPECIFIC section (change 'D' to whatever section you want to test):
   // if(sectionLayers?.['D5']) { const s = sectionLayers['D5']; if(s.route) s.route.addTo(map); if(s.outline) s.outline.addTo(map); }
