e8e87e3b928dfea8c01d7ee7202eaeea54bbdc95
thc225 <thc225@lehigh.edu>
2025-11-11 15:48:02 -0500

    useless commit

--- search/profile/profile.js
+++ search/profile/profile.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const qParam  = searchParams.get("q");
   const latParam = parseFloat(searchParams.get("lat"));
   const lngParam = parseFloat(searchParams.get("lng"));
-  const API_BASE_URL = 'https://niskyhillcemetery-23a1ead2d9b1.herokuapp.com';
+  const API_BASE_URL = 'https://www.niskyhill.org';
 
   // ===== Map state =====
   let map;
