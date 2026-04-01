4c2d674552312a9f3c7cce7d631182bff767d98a
Anthony Cutrona <ajc327@lehigh.edu>
2026-04-01 18:06:09 -0400

    Implemented fetchSections() function for dropdowns

--- admin/src/api/lotApi.js
+++ admin/src/api/lotApi.js
@@ -1,3 +1,21 @@
 //all API calls for lots will reside here, then will be utilized in thier respective react functions
 
-const API_BASE_URL = 'http://localhost:5173'; //Should update to env var
+const API_BASE_URL = 'http://localhost:8080';  //Should update to env var
+
+
+//gets all sections, returning as list for population in jsx files
+async function fetchSections(){
+    const response = await fetch(`${API_BASE_URL}/sections/all`)
+    if (!response.ok) {
+        throw new Error("Network response was not ok");
+    }
+
+    const data = await response.json();
+    console.log(data);
+    return data; // array of section json
+}
+
+
+
+
+export default fetchSections;
